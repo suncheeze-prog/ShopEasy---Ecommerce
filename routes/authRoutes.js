af553f942da6d06edcadd33f7b888ef7d982225c
suncheeze-prog <sanchezallen2003@gmail.com>
2026-04-03 11:21:23 +0800

    fix: change bcrypt to bcryptjs

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt"); // ✅ IMPORT MO TO!
+const bcrypt = require("bcryptjs"); // ✅ IMPORT MO TO!
 
 // -------------------------
 // POST /api/login
